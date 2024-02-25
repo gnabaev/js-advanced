7ef1cd24d3ced09c4f2a82dc1920180cf351701d
gnabaev <gnabaev@gmail.com>
2024-02-25 14:03:06 +0300

    Выполнено задание 3.

---
+++ 3-birthday/app.js
@@ -0,0 +1,14 @@
+function IsMore14(dateString) {
+    const date = new Date(dateString);
+    const dif = Date.now() - date.getTime();
+    const difDate = new Date(dif);
+    const difYear = difDate.getUTCFullYear();
+    var age = Math.abs(difYear - 1970);
+
+    if (age >= 14) {
+        return true;
+    }
+    return false;
+}
+
+console.log(IsMore14('2010-02-26'));
